40e5963b1019252bb59b76c7f43f0411e8576bea
Volkov Alexander <volkov.525@gmail.com>
2020-08-27 10:19:06 +0300

    added pug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+	root: true,
+	env: {
+		node: true,
+		browser: true,
+		es6: true
+	},
+	'extends': [
+		'plugin:vue/essential',
+		'eslint:recommended'
+		// '@vue/standard'
+	],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		// 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		// 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+		'indent': [ 'error', 'tab' ],
+		'no-tabs': 0,
+		'no-mixed-spaces-and-tabs': [ 'error', 'smart-tabs' ]
+	}
 }
